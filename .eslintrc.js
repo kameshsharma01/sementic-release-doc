f9856a440ecd00a1c9feb3ff49ddba088979ec89
kamesh-amdocs <root@DVCGNH6111944.corp.amdocs.com>
2022-06-24 14:35:25 +0530

    add dependency files

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    root: true,
+    env: {
+        es6: true,
+        node: true
+    },
+    parserOptions: {
+        ecmaVersion: 2020
+    },
+    extends: ['eslint:recommended', 'prettier'],
+    rules: {}
+};
